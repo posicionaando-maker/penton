d5605f834014b056dba8d306db1abbe5b6e3ca33
posicionaando-maker <posiciona.ando@gmail.com>
2026-07-25 05:04:30 -0400

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'ferreteria-v1';
+const ARCHIVOS = [
+    '/',
+    '/index.html',
+    '/css/style.css',
+    '/js/app.js',
+    '/manifest.json',
+    '/assets/img/logo-ferreteria.jpg',
+    // Agrega aquí imágenes clave que quieras cachear
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(ARCHIVOS))
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(res => res || fetch(event.request))
+    );
+});
